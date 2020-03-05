34c58181b42287ecfa16d577fe8b9261d0da62f1
marat <marat.barski@mdclone.com>
2020-03-05 12:20:37 +0200

    offline node

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   serverUrl: 'http://localhost:3000/',
   loginUrl: 'http://10.0.2.18:3000/login',
   socketUrl: 'http://localhost:4444',
-  isOfflineMode: true
+  isOfflineMode: false
 };
