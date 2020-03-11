0216ba48379f88b8a4b54a4a08f7fd1eba98b1bf
Marat <bmaratb@gmail.com>
2020-03-11 09:06:11 +0200

    imp module

--- src/app/imported-files/imported-files.module.ts
+++ src/app/imported-files/imported-files.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ImportedFilesComponent } from './pages/imported-files/imported-files.component';
 import { Route, RouterModule } from '@angular/router';
 import { CoreModule } from 'appcore';
-import { UploadFileComponent } from '../shared/components/upload-file/upload-file.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { StoreModule } from '@ngrx/store';
 import { fileSource } from './store/reducers/imported-files.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { SourceFileEffect } from './store/effects/imported-files-load.effect';
 import { MyQueriesComponent } from './pages/my-queries/my-queries.component';
-import { ProjectComboComponent } from '@app/shared/components/project-combo/project-combo.component';
 import { SharedModule } from '@app/shared/shared.module';
 
 const routes: Array<Route> = [
