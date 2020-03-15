40761defde5b4cc6ef22e7bb7cb94885f70c5077
Marat <bmaratb@gmail.com>
2020-03-15 19:04:27 +0200

    remove scroll modal window

--- projects/core/src/lib/components/modal-window/modal-window.component.ts
+++ projects/core/src/lib/components/modal-window/modal-window.component.ts
@@ -6,14 +6,23 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./modal-window.component.css']
 })
 export class ModalWindowComponent {
-  @Input() 
+
+  @Input() removeScroll = true;
+  @Input() topBorder = true;
+  @Input() shadow = true;
+  @Input('hidden')
+
   set isHidden(hidden: boolean) {
     this._isHidden = hidden;
-    if(this._isHidden){
-      
+    if (this.removeScroll) {
+      if (!this._isHidden) {
+        document.body.style.overflow = 'hidden';
+      } else {
+        document.body.style.overflow = 'visible';
+      }
     }
   }
   get isHidden(): boolean { return this._isHidden; }
-  _isHidden = false;
-  constructor() { }
+
+  private _isHidden = false;
 }
