b488ccdbd46217bd3ab580af564c4edb8d4d1833
Marat <bmaratb@gmail.com>
2020-03-15 13:16:06 +0200

    progress bar

--- projects/core/src/lib/components/progress/progress.component.ts
+++ projects/core/src/lib/components/progress/progress.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mdc-progress',
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.css']
 })
-export class ProgressComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ProgressComponent {
+  @Input() state = 0;
+  @Input() showMatch = false;
+  @Input() css = 'progress_green';//progress_red,progress_yellow
 }
