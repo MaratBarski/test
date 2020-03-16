ea16234013016d87e947f113306b373a8d790614
Marat <bmaratb@gmail.com>
2020-03-16 22:59:05 +0200

    scrol after navigation

--- projects/core/src/lib/components/side-bar/side-bar.component.ts
+++ projects/core/src/lib/components/side-bar/side-bar.component.ts
@@ -1,27 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ActivatedRouteSnapshot, Router, NavigationEnd } from '@angular/router';
 import { SideMenu, MenuItem } from '../../common/side-menu';
+import { ComponentService } from '../../services/component.service';
+import { BaseSibscriber } from '../../common/BaseSibscriber';
 
 @Component({
   selector: 'mdc-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css']
 })
-export class SideBarComponent implements OnInit {
+export class SideBarComponent extends BaseSibscriber implements OnInit {
 
-  constructor(private router: Router, private activeRouter: ActivatedRoute) { }
-  
+  constructor(private router: Router, private activeRouter: ActivatedRoute) {
+    super();
+  }
+
   items = SideMenu;
 
   ngOnInit() {
     // this.activeRouter.data.subscribe(data => {
     //   alert(JSON.stringify(data));
     // })
-    // this.router.events.subscribe(event => {
-    //   if (event instanceof NavigationEnd) {
-    //     alert(window.location.href);
-    //   }
-    // })
+    super.add(
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          //alert(window.location.href);
+          ComponentService.scrollTo(0, 0);
+        }
+      }));
   }
 
   isActive(item: MenuItem): boolean {
