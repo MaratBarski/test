ec55d5869bdf39cc9cc7238c1ac50c1f884bb0f7
Marat <bmaratb@gmail.com>
2020-03-22 20:43:03 +0200

    session history

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,6 +12,6 @@ export const environment = {
     uploadFileSource: 'mdclone/api/v1/upload/file-source',
     deleteFileSource: 'mdclone/api/v1/file-source',
     hierarchy: 'mdclone/api/v1/hierarchy',
-    historyReport: 'mdclone/api/v1/historyReport'
+    historyReport: 'mdclone/api/v1/session-history'
   }
 };
