5a39923896eab3fe72c74d69442cc00f161fae9d
MosheKamer <moshe.kamer@mdclone.com>
2020-03-26 11:55:00 +0200

    #add model session history

--- src/environments/environment.moshe.ts
+++ src/environments/environment.moshe.ts
@@ -3,7 +3,7 @@ export const environment = {
     production: false,
     serverUrl: 'http://10.0.2.18:4000/',
     //loginUrl: 'http://localhost:3000/login/',
-    loginUrl: 'http://10.0.2.18:3000/login',
+    loginUrl: 'http://10.0.2.18:4000/login',
     socketUrl: 'http://localhost:4444',
     isOfflineMode: true,
     endPoints: {
@@ -12,6 +12,6 @@ export const environment = {
         uploadFileSource: 'mdclone/api/v1/upload/file-source',
         deleteFileSource: 'mdclone/api/v1/file-source',
         hierarchy: 'mdclone/api/v1/hierarchy',
-        historyReport: 'mdclone/api/v1/session-history'
+        historyReport: 'moshe/api/v1/session-history'
     }
 };
