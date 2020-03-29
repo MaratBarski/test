7fe869adb52b8526ee30b62093d9b82a0676c786
Marat <bmaratb@gmail.com>
2020-03-29 11:01:49 +0300

    added models

---
+++ src/app/models/file-type-bank.ts
@@ -0,0 +1,5 @@
+export class FileTypeBank {
+    public fileTypeId: number;
+    public fileTypeName: string;
+    public insertDate: Date;
+}
---
+++ src/app/models/session-history.ts
@@ -0,0 +1,61 @@
+import { Template } from './template';
+import { Project } from './project';
+import { SessionHistoryFiles } from './session-history-files';
+import { UserSession } from './user-session';
+import { User } from 'core/lib/models/UserInfo';
+
+export class SessionHistoryResponse {
+     data?: Array<SessionHistory>;
+     massage?: string;
+     status?: string;
+}
+
+export class SessionHistoryMappingResponse {
+     data?: SessionHistory;
+     massage?: string;
+     status?: string;
+}
+
+
+export class SessionHistory {
+     sessionHistoryId: number;
+     userId: number;
+     sessionId: number;
+     fileId: string;
+     insertDate: string;
+     projectId: number;
+     userQeSession: string;
+     userActivateSession: any;
+     transStatus: boolean;
+     transMsg: string;
+     sessionName: string;
+     fileNameAlias: string;
+     projectName: string;
+     templateId: number;
+     templateName: string;
+     sessionHistoryFiles: SessionHistoryFiles[];
+     template: Template;
+     userSession: UserSession;
+     project: Project;
+     user: User;
+}
+
+
+// export class SessionHistory {
+//      fileId: number;
+//      fileName: string;
+//      filePath: string;
+//      fileNameAlias: string;
+//      tag: string;
+//      insertDate: Date;
+//      templateId: number;
+//      tableName: string;
+//      project: number;
+//      uploadedBy: number;
+//      fileType: number;
+//      projectObj?: Project;
+//      template?: Template;
+//      user?: User;
+//      rowsNum?: number;
+//      columnsNum?: number;
+// }
