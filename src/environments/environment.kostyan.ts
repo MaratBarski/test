4bb004421222e5de7ab50089bc5ee3dd8b961736
Konstantin Popkov <konstantin.popkov@mdclone.com>
2020-04-06 03:09:37 +0300

    added edit file

--- src/environments/environment.kostyan.ts
+++ src/environments/environment.kostyan.ts
@@ -1,17 +1,19 @@
 export const environment = {
-    name:'kostyan',
-    production: false,
-    serverUrl: 'http://10.0.2.18:4000/',
-    //loginUrl: 'http://localhost:3000/login/',
-    loginUrl: 'http://10.0.2.18:3000/login',
-    socketUrl: 'http://localhost:4444',
-    isOfflineMode: true,
-    endPoints: {
-        userData: 'user-data',
-        fileSource: 'mdclone/api/v1/file-source',
-        uploadFileSource: 'mdclone/api/v1/upload/file-source',
-        deleteFileSource: 'mdclone/api/v1/file-source',
-        hierarchy: 'mdclone/api/v1/hierarchy',
-        historyReport: 'mdclone/api/v1/session-history'
-    }
+  name: 'kostyan',
+  production: false,
+  serverUrl: 'http://10.0.2.18:4000/',
+  //loginUrl: 'http://localhost:3000/login/',
+  loginUrl: 'http://10.0.2.18:3000/login',
+  socketUrl: 'http://localhost:4444',
+  isOfflineMode: false,
+  endPoints: {
+    userData: 'user-data',
+    fileSource: 'mdclone/api/v1/file-source',
+    uploadFileSource: 'mdclone/api/v1/upload/file-source',
+    deleteFileSource: 'mdclone/api/v1/file-source',
+    hierarchy: 'mdclone/api/v1/hierarchy',
+    historyReport: 'mdclone/api/v1/session-history',
+    downloadHistoryReport: 'moshe/api/v1/session-history/download',
+    usageReport: 'mdclone/api/v1/reporting/usage'
+  }
 };
