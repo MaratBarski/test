d4c14142ffdc90782d56e69a39c229fa0c104fea
Marat <bmaratb@gmail.com>
2020-04-19 15:35:06 +0300

    add edit categories

---
+++ src/app/categorization/pages/edit-categories/edit-categories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'md-edit-categories',
+  templateUrl: './edit-categories.component.html',
+  styleUrls: ['./edit-categories.component.scss']
+})
+export class EditCategoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
