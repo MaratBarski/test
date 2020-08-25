4d3d2cf614ab8a20abb4805e78cc1a43b7c6cd43
Marat <bmaratb@gmail.com>
2020-08-25 16:56:49 +0300

    approvalKeyExpirationDate

--- src/app/users/services/permission-set.service.ts
+++ src/app/users/services/permission-set.service.ts
@@ -522,7 +522,14 @@ export class PermissionSetService extends BaseSibscriber {
       this._users = users.data;
       if (this._setId) {
         this._permissionSet = this.convertToClient(permSet.data);
-        this.showWarning = permSet.data.researchStatus && permSet.data.researchStatus.trim().toLowerCase() !== 'open';
+        //this.showWarning = permSet.data.researchStatus && permSet.data.researchStatus.trim().toLowerCase() !== 'open';
+        if (!permSet.data.approvalKeyExpirationDate) {
+          this.showWarning = false
+        } else {
+          if (new Date() > new Date(permSet.data.approvalKeyExpirationDate)) {
+            this.showWarning = true;
+          }
+        }
         this.initTemplates(permSet.data);
       } else {
         this._permissionSet = permSet;
