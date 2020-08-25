1c7496a3e53b2ec84c841c068aa4fb79804d791e
Marat <bmaratb@gmail.com>
2020-08-25 11:50:15 +0300

    loader

--- projects/core/src/lib/services/navigation.service.ts
+++ projects/core/src/lib/services/navigation.service.ts
@@ -1,13 +1,39 @@
 import { Injectable } from '@angular/core';
 import { SideMenu } from '../common/side-menu';
-import { Router } from '@angular/router';
+import { Router, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
+import { Subject } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavigationService {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationStart) {
+        // Show loading indicator
+      }
+
+      if (event instanceof NavigationEnd) {
+        this.afterNavigate();
+      }
+
+      if (event instanceof NavigationError) {
+        this.afterNavigate();
+      }
+    });
+  }
+
+  get onNavigationStart(): Observable<string> {
+    return this._onNavigationStart.asObservable();
+  }
+  get onNavigationEnd(): Observable<any> {
+    return this._onNavigationEnd.asObservable();
+  }
+
+  private _onNavigationStart = new Subject<string>();
+  private _onNavigationEnd = new Subject();
 
   set currentPageID(currentPageID: string) {
     SideMenu.filter(x => x.active).forEach(x => x.active = false);
@@ -22,6 +48,11 @@ export class NavigationService {
       return;
     }
     this.router.navigateByUrl(url);
+    this._onNavigationStart.next(url);
+  }
+
+  afterNavigate(): void {
+    this._onNavigationEnd.next();
   }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component, ViewChild, ElementRef, AfterViewInit, HostListener} from '@angular/core';
-import {ComponentService, TranslateService, BaseSibscriber, animation, NavigationService} from '@appcore';
-import {ConfigService} from './shared/services/config.service';
-import {environment} from '../environments/environment';
+import { Component, ViewChild, ElementRef, AfterViewInit, HostListener } from '@angular/core';
+import { ComponentService, TranslateService, BaseSibscriber, animation, NavigationService } from '@appcore';
+import { ConfigService } from './shared/services/config.service';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -11,16 +11,26 @@ import {environment} from '../environments/environment';
   ],
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent extends BaseSibscriber {
   uiRoute = environment.uiRoute;
   message;
   constructor(
     public componentService: ComponentService,
     public configService: ConfigService,
+    private navigationService: NavigationService
   ) {
+    super();
+    super.add(this.navigationService.onNavigationStart.subscribe((url) => {
+      this.isNavigating = true;
+    }));
+    super.add(this.navigationService.onNavigationEnd.subscribe((url) => {
+      this.isNavigating = false;
+    }));
   }
 
-  send(msg){
+  isNavigating = false;
+
+  send(msg) {
     this.message.next(msg);
   }
 }
